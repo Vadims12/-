0253d1dcb239e44ea502c9f4bd98550c6fce7b8b
Vadims12 <90962157+Vadims12@users.noreply.github.com>
2022-01-20 20:08:30 +0200

    Add files via upload

---
+++ Task1/Task1/MallardDuck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task1.Swim;
+
+namespace Task1
+{
+    class MallardDuck : Duck
+    {
+        public MallardDuck()
+            : base(new FlyingWithWings(), new DuckQuack(), new Swimable())
+        { }
+        public override void Display()
+        {
+            Console.WriteLine("Mallard Duck:");
+            base.Display();
+        }
+    }
+}
---
+++ Task1/Task1/WoodDuck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task1.Swim;
+
+namespace Task1
+{
+    class WoodDuck : Duck
+    {
+        public WoodDuck()
+            :base(new NoFly(), new NoQuack(), new NotSwimable())
+        { }
+
+        public override void Display()
+        {
+            Console.WriteLine("Wood duck");
+            base.Display();
+        }
+    }
+}
