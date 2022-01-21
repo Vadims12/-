dab7a7211acbe3cf9b9a5a1ae35115fe77d0eea2
Vadims12 <90962157+Vadims12@users.noreply.github.com>
2022-01-21 13:13:15 +0200

    Add files via upload

---
+++ Task3(Decorator)/Task3(Decorator)/Beverage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3_Decorator_
+{
+    public abstract class Beverage
+    {
+        protected string description = "";
+        public abstract double Cost();
+
+        public string GetDescription()
+        {
+            return description;
+        }
+    }
+}
---
+++ Task3(Decorator)/Task3(Decorator)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Task3_Decorator_.Coffee;
+using Task3_Decorator_.Condiments;
+
+namespace Task3_Decorator_
+{
+    class Program
+    {
+        static void Main()
+        {
+         
+            Beverage darkRoast = new DarkRoast();
+            darkRoast = new Milk(darkRoast);
+            darkRoast = new Whip(darkRoast);
+            Console.WriteLine($"{darkRoast.GetDescription()}:{darkRoast.Cost()}");
+           
+            Beverage houseBlend = new HouseBlend();
+            houseBlend = new Milk(houseBlend);
+            houseBlend = new Mocha(houseBlend);
+            Console.WriteLine($"{houseBlend.GetDescription()}:{houseBlend.Cost()}");
+      
+            Beverage espresso = new Espresso();
+            espresso = new Soy(espresso);
+            espresso = new Whip(espresso);
+            Console.WriteLine($"{espresso.GetDescription()}:{espresso.Cost()}");
+            Console.ReadLine();
+        }
+    }
+}
