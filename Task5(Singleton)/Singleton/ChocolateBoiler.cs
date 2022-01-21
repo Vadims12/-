812cf2efa3b6e7ce7eab4aa6bca40d2d941a2897
Vadims12 <90962157+Vadims12@users.noreply.github.com>
2022-01-21 11:25:51 +0200

    Add files via upload

---
+++ Task5(Singleton)/Singleton/ChocolateBoiler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class ChocolateBoiler
+    {
+        public bool Empty { get; private set; }
+        public bool Boiled { get; private set; }
+        private static ChocolateBoiler boiler;
+        private static object Lock = new object();
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public static ChocolateBoiler GetBoiler()
+        {
+            if (boiler == null)
+            {
+                lock (Lock)
+                {
+                    boiler = new ChocolateBoiler();
+                    {
+                        if (boiler == null)
+                        {
+                            boiler = new ChocolateBoiler();
+                        }
+                    }
+                }
+            }
+            return boiler;
+        } 
+
+        public void Fill()
+        {
+            if (Empty)
+            {
+                Empty = false;
+                Boiled = false;
+            }
+        }
+        public void Drain()
+        {
+            if (!Empty && Boiled)
+            {
+                Empty = true;
+                Boiled = false;
+            }
+        }
+        public void Boil()
+        {
+            if (!Empty && !Boiled)
+            {
+                Boiled = true;
+            }
+        }     
+    }
+}
---
+++ Task5(Singleton)/Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            ChocolateBoiler chocolateBoiler = ChocolateBoiler.GetBoiler();
+            chocolateBoiler.Fill();
+            chocolateBoiler.Boil();
+            chocolateBoiler.Drain();
+        }
+    }
+}
